87b610c5c140d615f1c3fdab3567124ae505ec86
julianopita <50183295+julianopita@users.noreply.github.com>
2024-08-09 13:47:47 -0300

    fixed color scheme change, fixed typos

--- javascript/mudar_tema.js
+++ javascript/mudar_tema.js
@@ -35,11 +35,16 @@ function mudarParaModoDark() {
     document.documentElement.style.setProperty('--cor-midia-social-footer', '#000000');
     document.documentElement.style.setProperty('--cor-quadrado-onda-fundo', '#333333');
     document.documentElement.style.setProperty('--cor-neutra-cinza-escuro', '#F5F5F5');
-    document.getElementById('wave').src = "../images/wave_dark.svg";
-    
-    if (document.getElementById("sobrenos-container") != undefined) {
-    document.getElementById("sobrenos-container").style.backgroundImage = "url('../images/wave_invertida_dark.svg')";
-    }
+    if (document.getElementById('wave') != undefined) {
+    document.getElementById('wave').src = "../images/wave_dark.svg"
+    } else {
+    document.getElementById('waveind').src = "images/wave_dark.svg" 
+    };
+    if (document.getElementById('content') != undefined) {
+    document.getElementById("content").style.backgroundImage = "url('../images/wave_invertida_dark.svg')"
+    } else {
+    document.getElementById("contentind").style.backgroundImage = "url('images/wave_invertida_dark.svg')"
+    };   
 }
 
 function mudarParaModoLight() {
@@ -52,11 +57,16 @@ function mudarParaModoLight() {
     document.documentElement.style.setProperty('--cor-midia-social-footer', '#e8eef2');
     document.documentElement.style.setProperty('--cor-quadrado-onda-fundo', '#e9a209');
     document.documentElement.style.setProperty('--cor-neutra-cinza-escuro', '#1d1d1d');
-    document.getElementById('wave').src = "../images/wave.svg";
-
-    if (document.getElementById("sobrenos-container") != undefined) {
-    document.getElementById("sobrenos-container").style.backgroundImage = "url('../images/wave_invertida_light.svg')";
-    }
+    if (document.getElementById('wave') != undefined) {
+    document.getElementById('wave').src = "../images/wave.svg"
+    } else {
+    document.getElementById('waveind').src = "images/wave.svg" 
+    };
+    if (document.getElementById('content') != undefined) {
+    document.getElementById("content").style.backgroundImage = "url('../images/wave_invertida_light.svg')"
+    } else {
+    document.getElementById("contentind").style.backgroundImage = "url('images/wave_invertida_light.svg')"
+    };   
 }
 
 
--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,120 @@
 $(document).ready(function() {
     $('#mobile_btn').on('click', function() {
         $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn').find('i').toggleClass('fa-x');
     });
+
+    const sections = $('section');
+    const navItems = $('.nav-item');
+
+    $(window).on('scroll', function () {
+        const header = $('header');
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+        let activeSectionIndex = 0;
+
+        if (scrollPosition <= 0) {
+            header.css('box-shadow', 'none');
+        } else {
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1');
+        }
+
+        sections.each(function(i) {
+            const section = $(this);
+            const sectionTop = section.offset().top - 96;
+            const sectionBottom = sectionTop+ section.outerHeight();
+
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                activeSectionIndex = i;
+                return false;
+            }
+        })
+
+        navItems.removeClass('active');
+        $(navItems[activeSectionIndex]).addClass('active');
+    });
+
+    ScrollReveal().reveal('#cta', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('.dish', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('#testimonial_chef', {
+        origin: 'left',
+        duration: 1000,
+        distance: '20%'
+    })
+
+    ScrollReveal().reveal('.feedback', {
+        origin: 'right',
+        duration: 1000,
+        distance: '20%'
+    })
+
+    ScrollReveal().reveal('#main-container', {
+        origin: 'right',
+        duration: 1000,
+        distance: '20%'
+    })
+
+    ScrollReveal().reveal('#dishes', {
+        origin: 'right',
+        duration: 1000,
+        distance: '20%'
+    })
+
+     ScrollReveal().reveal('#section1', {
+        origin: 'left',
+        duration: 1000,
+        distance: '20%'
+    })
+
+    ScrollReveal().reveal('.comentarios', {
+        origin: 'right',
+        duration: 1000,
+        distance: '20%'
+    })
+
+    ScrollReveal().reveal('#section2', {
+        origin: 'right',
+        duration: 1000,
+        distance: '20%'
+    })
+
+    ScrollReveal().reveal('#section3', {
+        origin: 'left',
+        duration: 1000,
+        distance: '20%'
+    })
+
+    ScrollReveal().reveal('#massaOptions', {
+        origin: 'left',
+        duration: 1000,
+        distance: '20%'
+    })
+
+    ScrollReveal().reveal('#recheio1Options', {
+        origin: 'right',
+        duration: 1000,
+        distance: '20%'
+    })
+
+    ScrollReveal().reveal('#recheio2Options', {
+        origin: 'right',
+        duration: 1000,
+        distance: '20%'
+    })
+
+    ScrollReveal().reveal('#coberturaOptions', {
+        origin: 'left',
+        duration: 1000,
+        distance: '20%'
+    })
 });
